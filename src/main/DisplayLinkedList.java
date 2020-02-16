d5904d517b411624ea869f73ade8299091e98911
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2020-02-16 21:47:23 +0530

    display the element and position in the linked list

---
+++ src/main/DisplayLinkedList.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.*;
+public class DisplayLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> list_Strings=new LinkedList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        list_Strings.add("Pink");
+        System.out.println("Original List:"+list_Strings);
+        for(int p=0;p<list_Strings.size();p++){
+            System.out.println("The linked List:"+list_Strings.get(p));}
+    }
+}
