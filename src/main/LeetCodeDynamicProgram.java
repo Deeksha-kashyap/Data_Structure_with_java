cb2a4dbaa806b52efbbb31979d91fcb77a95a27f
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2020-02-16 22:26:32 +0530

    combinations[amount]+=combinations[amount-coin]

---
+++ src/main/LeetCodeDynamicProgram.java
@@ -0,0 +1,27 @@
+package main;
+import java.util.*;
+public class LeetCodeDynamicProgram {
+    public static void main(String[] args) {
+        System.out.println(change(12,new int[]{1,2,5}));
+    }
+    public static int change(int amount,int[] coins)
+    {
+        int[] combinations=new int[amount+1];
+        combinations[0]=1;
+        for(int coin:coins){
+            for(int i=1;i<combinations.length;i++){
+                if(i>=coin){
+                    combinations[i]+=combinations[i-coin];
+                }
+            }
+        }
+        return combinations[amount];
+    }
+    public static void printAmount(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    }
+}
