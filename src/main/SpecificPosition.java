b9270a7a657b06778727eddb29b65d883f2e1879
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2020-02-16 21:39:09 +0530

    insert at a specific position

--- src/main/SpecificPosition.java
+++ src/main/SpecificPosition.java
@@ -1,6 +1,6 @@
 package main;
 import java.util.*;
-public class EndLinkedList {
+public class SpecificPosition {
     public static void main(String[] args) {
         LinkedList<String> list_Strings=new LinkedList<String>();
         list_Strings.add("Red");
@@ -9,9 +9,12 @@ public class EndLinkedList {
         list_Strings.add("White");
         list_Strings.add("Black");
         list_Strings.add("Pink");
-        System.out.println("Original List:"+list_Strings);
+        System.out.println("Original List:");
+        LinkedList<String> list=new LinkedList<String>();
+        list.add("white");
+        list_Strings.addAll(1,list);
         //list_Strings.addFirst("white");
-        list_Strings.offerLast("yellow");
+        // list_Strings.offerLast("yellow");
         System.out.println("The linked List:"+list_Strings);
     }
 }
