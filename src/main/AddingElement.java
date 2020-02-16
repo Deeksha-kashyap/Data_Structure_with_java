452f68047a350d62222b56bd84f396a35f01fec9
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2020-02-16 19:35:29 +0530

    add the element in the list

--- src/main/AddingElement.java
+++ src/main/AddingElement.java
@@ -3,5 +3,10 @@ import java.util.*;
 public class AddingElement {
     public static void main(String[] args) {
      List<String> list_Strings=new ArrayList<String>();
+     list_Strings.add("Red");
+     list_Strings.add("Green");
+     list_Strings.add("Orange");
+     list_Strings.add("White");
+     list_Strings.add("Black");
     }
 }
