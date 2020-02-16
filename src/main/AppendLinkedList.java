873a0db9f1831b0928b1a487a3acc7c219b1104d
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2020-02-16 21:18:55 +0530

    append or create the specfied values

---
+++ src/main/AppendLinkedList.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.*;
+
+public class AppendLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> list_Strings=new LinkedList<String>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        list_Strings.add("Pink");
+        System.out.println("Original List:"+list_Strings);
+    }
+}
